585f765c07506ae9e055fee02d69d50e0d8709e3
Samantha Finley <srfinley@uchicago.edu>
2020-03-16 16:57:30 -0400

    selection, bubble, and count done

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -5,12 +5,18 @@ def selection_sort( arr ):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
+        # (hint, can do in 3 loc)
+        for j in range(cur_index, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
              
 
 
 
         # TO-DO: swap
+        temp = arr[cur_index]
+        arr[cur_index] = arr[smallest_index]
+        arr[smallest_index] = temp
 
 
 
@@ -18,13 +24,34 @@ def selection_sort( arr ):
     return arr
 
 
-# TO-DO:  implement the Bubble Sort function below
+# implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    for _ in range(len(arr)):
+        count = 0
+        for i in range(len(arr) - 1):
+            if arr[i] > arr[i+1]:
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+                count += 1
+        if count == 0:
+            break
     return arr
 
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
+    # assumes minimum value of 0 or greater
+    # I don't like setting default max=-1, why not just find the max here?
+    count = [0 for _ in range(maximum+1)]
+
+    for value in arr:
+        count[value] += 1
+    
+    arr = []
+    for i in range(len(count)):
+        while count[i] > 0:
+            arr.append(i)
+            count[i] -= 1
 
     return arr
