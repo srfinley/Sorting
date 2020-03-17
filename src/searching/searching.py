d0f4674e1cdf431c659ce1b56525436f1d4fb3da
Samantha Finley <srfinley@uchicago.edu>
2020-03-17 11:55:19 -0400

    linear search and recursive binary pass tests

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,30 +1,46 @@
 # STRETCH: implement Linear Search				
 def linear_search(arr, target):
   
-  # TO-DO: add missing code
+    found = -1
+    for i, item in enumerate(arr):
+        if item == target:
+            found = i
+            break
 
-   return -1   # not found
+    return found   # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
 def binary_search(arr, target):
 
-  if len(arr) == 0:
-    return -1 # array empty
+    if len(arr) == 0:
+        return -1 # array empty
     
-  low = 0
-  high = len(arr)-1
+    low = 0
+    high = len(arr)-1
 
-  # TO-DO: add missing code
+    # TO-DO: add missing code
 
-  return -1 # not found
+    return -1 # not found
 
 
 # STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
   
-  middle = (low+high)//2
+    middle = (low+high)//2
 
-  if len(arr) == 0:
-    return -1 # array empty
-  # TO-DO: add missing if/else statements, recursive calls
+    if len(arr) == 0:
+        return -1 # array empty YEET
+
+    if len(arr) == 1:
+        if arr[0] == target:
+            return middle
+        else:
+            return -1
+
+    if arr[middle] == target:
+        return middle
+    elif arr[middle] > target:
+        return binary_search_recursive(arr, target, low, middle)
+    else:
+        return binary_search_recursive(arr, target, middle, high)
